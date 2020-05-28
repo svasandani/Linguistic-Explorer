9952681799b357a048f30a66322e7d0ff436dc96
Shailesh Vasandani <vasandanishailesh@gmail.com>
2020-05-28 14:22:42 -0400

    Removed server.pid that was stopping container from starting

--- docker-entry.sh
+++ docker-entry.sh
@@ -3,5 +3,8 @@
 # compile static assets
 bundle exec rake assets:clobber && bundle exec rake assets:precompile
 
+# remove the server pid
+rm -f tmp/pids/server.pid
+
 # start the server
 bundle exec rails s -b 0.0.0.0
